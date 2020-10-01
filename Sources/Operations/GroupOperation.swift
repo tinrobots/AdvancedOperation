706722351c23247c2a629ab29de2217e93474c70
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-10-01 14:58:54 +0200

    cleaning

--- Sources/Operations/GroupOperation.swift
+++ Sources/Operations/GroupOperation.swift
@@ -6,7 +6,7 @@ import Foundation
 /// Use a `GroupOperation` to associate related operations together, thereby creating higher levels of abstractions.
 open class GroupOperation: AsynchronousOperation {
   // MARK: - Public Properties
-  
+
   /// The maximum number of queued operations that can execute at the same time inside the `GroupOperation`.
   ///
   /// The value in this property affects only the operations that the current GroupOperation has executing at the same time.
@@ -15,7 +15,7 @@ open class GroupOperation: AsynchronousOperation {
     get { operationQueue.maxConcurrentOperationCount }
     set { operationQueue.maxConcurrentOperationCount = newValue }
   }
-  
+
   /// The relative amount of importance for granting system resources to the operation.
   public override var qualityOfService: QualityOfService {
     get { operationQueue.qualityOfService }
@@ -24,9 +24,9 @@ open class GroupOperation: AsynchronousOperation {
       operationQueue.qualityOfService = newValue
     }
   }
-  
+
   // MARK: - Private Properties
-  
+
   private let dispatchGroup = DispatchGroup()
   private let dispatchQueue = DispatchQueue(label: "\(identifier).GroupOperation.serialQueue")
 
@@ -34,9 +34,9 @@ open class GroupOperation: AsynchronousOperation {
     $0.isSuspended = true
     return $0
   }(OperationQueue())
-  
+
   // MARK: - Initializers
-  
+
   /// Creates a new `GroupOperation`.
   /// - Parameters:
   ///   - underlyingQueue: The dispatch queue used to execute operations (the default value is nil).
@@ -46,7 +46,7 @@ open class GroupOperation: AsynchronousOperation {
     self.operationQueue.underlyingQueue = underlyingQueue
     operations.forEach { addOperation($0) }
   }
-  
+
   /// Creates a new `GroupOperation`.
   /// - Parameters:
   ///   - underlyingQueue: The dispatch queue used to execute operations (the default value is nil).
@@ -54,9 +54,9 @@ open class GroupOperation: AsynchronousOperation {
   public convenience init(underlyingQueue: DispatchQueue? = nil, operations: Operation...) {
     self.init(underlyingQueue: underlyingQueue, operations: operations)
   }
-  
+
   // MARK: - Public Methods
-  
+
   ///  The default implementation of this method executes the scheduled operations.
   ///  If you override this method to perform the desired task,  invoke super in your implementation as last statement.
   ///  This method will automatically execute within an autorelease pool provided by Operation, so you do not need to create your own autorelease pool block in your implementation.
@@ -64,17 +64,17 @@ open class GroupOperation: AsynchronousOperation {
     if #available(iOS 13.0, iOSApplicationExtension 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *) {
       progress.addChild(operationQueue.progress, withPendingUnitCount: 1)
     }
-    
+
     guard !isCancelled else {
       self.finish()
       return
     }
-    
+
     // Debug only: count how many tasks have entered the dispatchGroup
     // let entersCount = dispatchGroup.debugDescription
     // .components(separatedBy: ",").filter({$0.contains("count")}).first?
     // .components(separatedBy: CharacterSet.decimalDigits.inverted).compactMap{Int($0)}.first
-    
+
     // 1. configuration started: enter the group
     // Without entering the group here, the notify block could be called before firing the queue if no operations were added.
     dispatchGroup.enter()
@@ -88,7 +88,7 @@ open class GroupOperation: AsynchronousOperation {
     // 4. configuration finished: leave the group
     dispatchGroup.leave()
   }
-  
+
   public final override func cancel() {
     dispatchQueue.sync {
       super.cancel()
@@ -97,7 +97,7 @@ open class GroupOperation: AsynchronousOperation {
       // and the operations will be cancelled without having a chance to finish.
     }
   }
-  
+
   /// Adds new `operations` to the `GroupOperation`.
   ///
   /// If the `GroupOperation` is already cancelled,  the new  operations will be cancelled before being added.
@@ -105,30 +105,30 @@ open class GroupOperation: AsynchronousOperation {
   public func addOperations(_ operations: Operation...) {
     dispatchQueue.sync {
       guard !isFinished else { return }
-      
+
       operations.forEach { operation in
         // 1. observe when the operation finishes
         dispatchGroup.enter()
         operation.addObserver(self, forKeyPath: #keyPath(Operation.isFinished), options: [.new, .old], context: nil)
-        
+
         // If the GroupOperation is cancelled, operations will be cancelled before being added to the queue.
         if isCancelled {
           operation.cancel()
         } else {
           // 2. observe when the operation gets cancelled if it's not cancelled yet
           operation.addObserver(self, forKeyPath: #keyPath(Operation.isCancelled), options: [.new, .old], context: nil)
-          
+
           // the progress totalUnitCount is increased by 1 only if the operation is not cancelled
           if #available(iOS 13.0, iOSApplicationExtension 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *) {
             operationQueue.progress.totalUnitCount += 1
           }
         }
-        
+
         operationQueue.addOperation(operation)
       }
     }
   }
-  
+
   /// Adds a new `operation` to the `GroupOperation`.
   ///
   /// If the `GroupOperation` is already cancelled,  the new  operation will be cancelled before being added.
@@ -136,14 +136,16 @@ open class GroupOperation: AsynchronousOperation {
   public final func addOperation(_ operation: Operation) {
     addOperations(operation)
   }
-  
-  open override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+
+  // swiftlint:disable:next block_based_kvo
+  open override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+    // The Swift KVO system causes crashes during tests
     guard let operation = object as? Operation else { return }
     guard let oldValue = change?[NSKeyValueChangeKey.oldKey] as? Bool else { return }
     guard let newValue = change?[NSKeyValueChangeKey.newKey] as? Bool else { return }
     guard let keyPath = keyPath else { return }
-    guard  newValue != oldValue else { return }
-    
+    guard newValue != oldValue else { return }
+
     switch keyPath {
     case #keyPath(Operation.isFinished):
       operation.removeObserver(self, forKeyPath: keyPath)
